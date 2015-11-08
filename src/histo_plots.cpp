3c7687c8edd5eef4e623c98523b3e0ea8ea522c7
Annika Monari <annikamonari@gmail.com>
2015-11-08 17:08:29 +0000

    added mroe files for analysis

--- src/histo_plots.cpp
+++ src/histo_plots.cpp
@@ -23,23 +23,23 @@ public:
     tree->SetBranchAddress("lumi", &lumi);
     tree->SetBranchAddress("event", &event);
 
-    TH1D* histo1D = new TH1D("lumi","Luminosity", 180,0,1800);
+    TH2D* histo2D = new TH2D("lumi vs event","Luminosity vs Events", 180,0,1800, 1000,0,1000000000);
 
     for(int i = 0; i < nentries; i++) {
       tree->GetEntry(i);
-      histo1D->Fill(lumi);
+      histo2D->Fill(lumi, event);
     }
 
     TCanvas *c2 = new TCanvas("c2","lumi plot");
-    Set1DHistoStyle(histo1D, 2, 3);
-    histo1D->Draw();
+    Set1DHistoStyle(histo2D, 2, 3);
+    histo2D->Draw();
     c2->Update();
   }
 };
 
 int main(int argc, char** argv) {
   TApplication theApp("tapp", &argc, argv);
-  TFile* f = new TFile("docs/PARKED_VBF-Parked-2012B-22Jan2013-v1-1.root");
+  TFile* f = new TFile("docs/PARKED_VBF-Parked-2012B-22Jan2013-v1-0.root");
   TTree* t = (TTree*) f->Get("LightTree");
   Plot* plot = new Plot(t);
   

ab229a08d200df2c2523331641b4cf44995efcdd
Annika Monari <annikamonari@gmail.com>
2015-11-08 14:37:57 +0000

    got plots working

--- include/Plotting_style.h
+++ include/Plotting_style.h
@@ -3,14 +3,15 @@
 
 #include <TROOT.h>
 #include <TStyle.h>
+#include <TH2.h>
 
 // Set style for 1D histogram
-void Set1DHistoStyle(int lineColour = 1, int lineWidth = 1, int fillColour = 0);
+void Set1DHistoStyle(TH1D* histo1d, int lineColour = 1, int lineWidth = 1, int fillColour = 0);
 
-void Set1DHistoStyle(int lineColour, int lineWidth, int fillColour) {
-  gStyle->SetLineColor(lineColour);
-  gStyle->SetLineWidth(lineWidth);
-  gStyle->SetFillColor(fillColour);
+void Set1DHistoStyle(TH1D* histo1d, int lineColour, int lineWidth, int fillColour) {
+  histo1d->SetLineColor(lineColour);
+  histo1d->SetLineWidth(lineWidth);
+  histo1d->SetFillColor(fillColour);
   gROOT->ForceStyle();
 }
 
--- src/histo_plots.cpp
+++ src/histo_plots.cpp
@@ -4,6 +4,7 @@
 #include <TChain.h>
 #include <TFile.h>
 #include <TH2.h>
+#include <TApplication.h>
 
 class Plot {
   TTree* tree;
@@ -15,30 +16,35 @@ public:
     nentries = tree->GetEntries();
   }
 
-  void plot_lumi_vs_events() {
+  void plot_lumi() {
     UInt_t lumi;
     UInt_t event;
 
     tree->SetBranchAddress("lumi", &lumi);
     tree->SetBranchAddress("event", &event);
 
-    Set1DHistoStyle(4, 2);
-    TH2D* histo2D = new TH2D("eventvslumi","Events vs Luminosity", 500,0,500, 1000,0,10000000);
+    TH1D* histo1D = new TH1D("lumi","Luminosity", 180,0,1800);
 
     for(int i = 0; i < nentries; i++) {
       tree->GetEntry(i);
-      histo2D->Fill(event, lumi);
+      histo1D->Fill(lumi);
     }
 
-    TCanvas *c2 = new TCanvas("c2","event vs lumi plot");
-    histo2D->Draw();
+    TCanvas *c2 = new TCanvas("c2","lumi plot");
+    Set1DHistoStyle(histo1D, 2, 3);
+    histo1D->Draw();
+    c2->Update();
   }
 };
 
-int main(void) {
+int main(int argc, char** argv) {
+  TApplication theApp("tapp", &argc, argv);
   TFile* f = new TFile("docs/PARKED_VBF-Parked-2012B-22Jan2013-v1-1.root");
   TTree* t = (TTree*) f->Get("LightTree");
   Plot* plot = new Plot(t);
-
-  plot->plot_lumi_vs_events();
+  
+  plot->plot_lumi();
+  theApp.Run();
+  
+  return 0;
 };

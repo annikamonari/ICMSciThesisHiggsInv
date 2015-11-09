6af86ee90e87f1bf2100173b4e601d9419e33ce0
Annika Monari <annikamonari@gmail.com>
2015-11-09 17:14:48 +0000

    changes to the main function

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@ int main(int argc, char** argv) {
 
   DataTree* tree1 = new DataTree(new TFile("docs/PARKED_VBF-Parked-2012B-22Jan2013-v1-0.root"), "Data Run 0");
   DataTree* tree2 = new DataTree(new TFile("docs/PARKED_VBF-Parked-2012B-22Jan2013-v1-2.root"), "Data Run 2");
-  TCut cut = "lumi < 1000";
+  TCut cut = NULL;
 
-  draw_stacked_histoplots({tree1, tree2}, "lumi", &cut);
+  draw_stacked_histoplots({tree1, tree2}, "jet1_eta", &cut);
 
   theApp.Run();
   

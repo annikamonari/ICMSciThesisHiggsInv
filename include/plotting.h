02ba25d491f619608a6de7cddce01a4a9012a01b
Annika Monari <annikamonari@gmail.com>
2015-11-10 16:17:41 +0000

    some changes to code, deleted unnecessary MCs

--- include/plotting.h
+++ include/plotting.h
@@ -13,11 +13,11 @@
 void draw_stacked_histoplots(std::vector<DataTree*> data_trees, char* variable_name, char* selection) {
   TCanvas* c1 = new TCanvas("c1", "stacked hists");
   THStack hs("Stacked Histogram", variable_name);
-  TLegend* legend = new TLegend(0.6,0.7,0.9,0.9);
+  TLegend* legend = new TLegend(0.6,0.4,0.9,0.9);
   legend->SetTextSize(0.04);
   legend->SetBorderSize(0);
 
-  int colours[7] = {41, 43, 30, 40, 38, 11, 16};
+  int colours[11] = {41, 43, 30, 40, 38, 11, 16, 32, 22, 24, 13};
 
   for(int i = 0; i < data_trees.size(); i++) {
     TH1F* single_histo = data_trees[i]->create_histo_for_stack(colours[i], variable_name, selection);
@@ -26,12 +26,21 @@ void draw_stacked_histoplots(std::vector<DataTree*> data_trees, char* variable_n
     legend->AddEntry(single_histo, data_trees[i]->label, "f");
   }
 
+  std::string file_name(variable_name);
+  file_name.append("_");
+  file_name.append(selection);
+  file_name.append(".png");
+
   hs.Draw();
   //hs.GetYaxis()->SetTitle("Frequency");
   hs.GetXaxis()->SetTitle(variable_name);
+  hs.GetXaxis()->SetRangeUser(6, 12);
   c1->Update();
   legend->Draw();
   c1->Update();
+
+  c1->SaveAs(file_name.c_str());
+  c1->Close();
 }
 
 #endif

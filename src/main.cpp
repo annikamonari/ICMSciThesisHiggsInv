11456923ad0700c9a3124504713f2af244253ddf
dangordon1 <dsg12@ic.ac.uk>
2015-11-10 10:10:23 +0000

    changes to plotting

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,12 @@ int main(int argc, char** argv) {
   TApplication theApp("tapp", &argc, argv);
 
   //DataTree* tree1 = new DataTree(new TFile("docs/PARKED_VBF-Parked-2012B-22Jan2013-v1-0.root"), "Data Run 0");
-DataTree* tree1 = new DataTree(new TFile("docs/MC_DY1JetsToLL_iglep.root"), "MC_DY1JetsToLL_iglep.root");
-DataTree* tree2 = new DataTree(new TFile("docs/MC_DY1JetsToLL.root"), "MC_DY1JetsToLL.root");
-
+  DataTree* tree1 = new DataTree(new TFile("docs/PARKED_VBF-Parked-2012B-22Jan2013-v1-0.root");
   TCut cut = NULL;
 
-  draw_stacked_histoplots({tree1, tree2}, "jet1_eta", &cut);
+  draw_stacked_histoplots({tree1}, "jet1_eta", &cut);
 
   theApp.Run();
   
   return 0;
-}
+};

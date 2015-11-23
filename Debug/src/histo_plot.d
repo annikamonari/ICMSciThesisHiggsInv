ab977ad54a04a150b7c84c2ea75630aad3d726b8
Annika Monari <annikamonari@gmail.com>
2015-11-23 17:29:21 +0000

    fixed legend bugs

---
+++ Debug/src/histo_plot.d
@@ -0,0 +1,260 @@
+src/histo_plot.o: ../src/histo_plot.cpp ../src/../include/histo_plot.h \
+  /Applications/root_v5.34.34/include/TStyle.h \
+  /Applications/root_v5.34.34/include/TNamed.h \
+  /Applications/root_v5.34.34/include/TObject.h \
+  /Applications/root_v5.34.34/include/Rtypes.h \
+  /Applications/root_v5.34.34/include/RConfig.h \
+  /Applications/root_v5.34.34/include/RVersion.h \
+  /Applications/root_v5.34.34/include/DllImport.h \
+  /Applications/root_v5.34.34/include/Rtypeinfo.h \
+  /Applications/root_v5.34.34/include/snprintf.h \
+  /Applications/root_v5.34.34/include/strlcpy.h \
+  /Applications/root_v5.34.34/include/TGenericClassInfo.h \
+  /Applications/root_v5.34.34/include/TSchemaHelper.h \
+  /Applications/root_v5.34.34/include/TStorage.h \
+  /Applications/root_v5.34.34/include/TVersionCheck.h \
+  /Applications/root_v5.34.34/include/Riosfwd.h \
+  /Applications/root_v5.34.34/include/TBuffer.h \
+  /Applications/root_v5.34.34/include/TString.h \
+  /Applications/root_v5.34.34/include/TMathBase.h \
+  /Applications/root_v5.34.34/include/TAttAxis.h \
+  /Applications/root_v5.34.34/include/TAttLine.h \
+  /Applications/root_v5.34.34/include/TAttFill.h \
+  /Applications/root_v5.34.34/include/TAttText.h \
+  /Applications/root_v5.34.34/include/TAttMarker.h \
+  /Applications/root_v5.34.34/include/TArrayI.h \
+  /Applications/root_v5.34.34/include/TArray.h \
+  /Applications/root_v5.34.34/include/THStack.h \
+  /Applications/root_v5.34.34/include/TH1.h \
+  /Applications/root_v5.34.34/include/TAxis.h \
+  /Applications/root_v5.34.34/include/TArrayD.h \
+  /Applications/root_v5.34.34/include/TArrayC.h \
+  /Applications/root_v5.34.34/include/TArrayS.h \
+  /Applications/root_v5.34.34/include/TArrayF.h \
+  /Applications/root_v5.34.34/include/Foption.h \
+  /Applications/root_v5.34.34/include/TVectorFfwd.h \
+  /Applications/root_v5.34.34/include/TVectorDfwd.h \
+  /Applications/root_v5.34.34/include/TFitResultPtr.h \
+  /Applications/root_v5.34.34/include/TObjArray.h \
+  /Applications/root_v5.34.34/include/TSeqCollection.h \
+  /Applications/root_v5.34.34/include/TCollection.h \
+  /Applications/root_v5.34.34/include/TIterator.h \
+  /Applications/root_v5.34.34/include/TLegend.h \
+  /Applications/root_v5.34.34/include/TPave.h \
+  /Applications/root_v5.34.34/include/TBox.h \
+  /Applications/root_v5.34.34/include/TAttBBox2D.h \
+  /Applications/root_v5.34.34/include/GuiTypes.h \
+  /Applications/root_v5.34.34/include/TPoint.h \
+  ../src/../include/variable.h ../src/../include/data_chain.h \
+  /Applications/root_v5.34.34/include/TCanvas.h \
+  /Applications/root_v5.34.34/include/TPad.h \
+  /Applications/root_v5.34.34/include/TVirtualPad.h \
+  /Applications/root_v5.34.34/include/TAttPad.h \
+  /Applications/root_v5.34.34/include/TVirtualX.h \
+  /Applications/root_v5.34.34/include/Buttons.h \
+  /Applications/root_v5.34.34/include/TQObject.h \
+  /Applications/root_v5.34.34/include/TAttCanvas.h \
+  /Applications/root_v5.34.34/include/TCanvasImp.h \
+  /Applications/root_v5.34.34/include/TROOT.h \
+  /Applications/root_v5.34.34/include/TDirectory.h \
+  /Applications/root_v5.34.34/include/TList.h \
+  /Applications/root_v5.34.34/include/TDatime.h \
+  /Applications/root_v5.34.34/include/TUUID.h \
+  /Applications/root_v5.34.34/include/TFile.h \
+  /Applications/root_v5.34.34/include/TDirectoryFile.h \
+  /Applications/root_v5.34.34/include/TMap.h \
+  /Applications/root_v5.34.34/include/THashTable.h \
+  /Applications/root_v5.34.34/include/TUrl.h \
+  /Applications/root_v5.34.34/include/TApplication.h \
+  /Applications/root_v5.34.34/include/TApplicationImp.h \
+  /Applications/root_v5.34.34/include/TChain.h \
+  /Applications/root_v5.34.34/include/TTree.h \
+  /Applications/root_v5.34.34/include/TBranch.h \
+  /Applications/root_v5.34.34/include/TDataType.h \
+  /Applications/root_v5.34.34/include/TDictionary.h \
+  /Applications/root_v5.34.34/include/Property.h \
+  /Applications/root_v5.34.34/include/ESTLType.h \
+  /Applications/root_v5.34.34/include/TClass.h \
+  /Applications/root_v5.34.34/include/TObjString.h \
+  /Applications/root_v5.34.34/include/ThreadLocalStorage.h \
+  /Applications/root_v5.34.34/include/RConfigure.h \
+  /Applications/root_v5.34.34/include/TVirtualTreePlayer.h \
+  /Applications/root_v5.34.34/include/TCut.h \
+  /Applications/root_v5.34.34/include/TH2.h \
+  /Applications/root_v5.34.34/include/TMatrixFBasefwd.h \
+  /Applications/root_v5.34.34/include/TMatrixDBasefwd.h \
+  ../src/../include/tree_leaves.h
+
+../src/../include/histo_plot.h:
+
+/Applications/root_v5.34.34/include/TStyle.h:
+
+/Applications/root_v5.34.34/include/TNamed.h:
+
+/Applications/root_v5.34.34/include/TObject.h:
+
+/Applications/root_v5.34.34/include/Rtypes.h:
+
+/Applications/root_v5.34.34/include/RConfig.h:
+
+/Applications/root_v5.34.34/include/RVersion.h:
+
+/Applications/root_v5.34.34/include/DllImport.h:
+
+/Applications/root_v5.34.34/include/Rtypeinfo.h:
+
+/Applications/root_v5.34.34/include/snprintf.h:
+
+/Applications/root_v5.34.34/include/strlcpy.h:
+
+/Applications/root_v5.34.34/include/TGenericClassInfo.h:
+
+/Applications/root_v5.34.34/include/TSchemaHelper.h:
+
+/Applications/root_v5.34.34/include/TStorage.h:
+
+/Applications/root_v5.34.34/include/TVersionCheck.h:
+
+/Applications/root_v5.34.34/include/Riosfwd.h:
+
+/Applications/root_v5.34.34/include/TBuffer.h:
+
+/Applications/root_v5.34.34/include/TString.h:
+
+/Applications/root_v5.34.34/include/TMathBase.h:
+
+/Applications/root_v5.34.34/include/TAttAxis.h:
+
+/Applications/root_v5.34.34/include/TAttLine.h:
+
+/Applications/root_v5.34.34/include/TAttFill.h:
+
+/Applications/root_v5.34.34/include/TAttText.h:
+
+/Applications/root_v5.34.34/include/TAttMarker.h:
+
+/Applications/root_v5.34.34/include/TArrayI.h:
+
+/Applications/root_v5.34.34/include/TArray.h:
+
+/Applications/root_v5.34.34/include/THStack.h:
+
+/Applications/root_v5.34.34/include/TH1.h:
+
+/Applications/root_v5.34.34/include/TAxis.h:
+
+/Applications/root_v5.34.34/include/TArrayD.h:
+
+/Applications/root_v5.34.34/include/TArrayC.h:
+
+/Applications/root_v5.34.34/include/TArrayS.h:
+
+/Applications/root_v5.34.34/include/TArrayF.h:
+
+/Applications/root_v5.34.34/include/Foption.h:
+
+/Applications/root_v5.34.34/include/TVectorFfwd.h:
+
+/Applications/root_v5.34.34/include/TVectorDfwd.h:
+
+/Applications/root_v5.34.34/include/TFitResultPtr.h:
+
+/Applications/root_v5.34.34/include/TObjArray.h:
+
+/Applications/root_v5.34.34/include/TSeqCollection.h:
+
+/Applications/root_v5.34.34/include/TCollection.h:
+
+/Applications/root_v5.34.34/include/TIterator.h:
+
+/Applications/root_v5.34.34/include/TLegend.h:
+
+/Applications/root_v5.34.34/include/TPave.h:
+
+/Applications/root_v5.34.34/include/TBox.h:
+
+/Applications/root_v5.34.34/include/TAttBBox2D.h:
+
+/Applications/root_v5.34.34/include/GuiTypes.h:
+
+/Applications/root_v5.34.34/include/TPoint.h:
+
+../src/../include/variable.h:
+
+../src/../include/data_chain.h:
+
+/Applications/root_v5.34.34/include/TCanvas.h:
+
+/Applications/root_v5.34.34/include/TPad.h:
+
+/Applications/root_v5.34.34/include/TVirtualPad.h:
+
+/Applications/root_v5.34.34/include/TAttPad.h:
+
+/Applications/root_v5.34.34/include/TVirtualX.h:
+
+/Applications/root_v5.34.34/include/Buttons.h:
+
+/Applications/root_v5.34.34/include/TQObject.h:
+
+/Applications/root_v5.34.34/include/TAttCanvas.h:
+
+/Applications/root_v5.34.34/include/TCanvasImp.h:
+
+/Applications/root_v5.34.34/include/TROOT.h:
+
+/Applications/root_v5.34.34/include/TDirectory.h:
+
+/Applications/root_v5.34.34/include/TList.h:
+
+/Applications/root_v5.34.34/include/TDatime.h:
+
+/Applications/root_v5.34.34/include/TUUID.h:
+
+/Applications/root_v5.34.34/include/TFile.h:
+
+/Applications/root_v5.34.34/include/TDirectoryFile.h:
+
+/Applications/root_v5.34.34/include/TMap.h:
+
+/Applications/root_v5.34.34/include/THashTable.h:
+
+/Applications/root_v5.34.34/include/TUrl.h:
+
+/Applications/root_v5.34.34/include/TApplication.h:
+
+/Applications/root_v5.34.34/include/TApplicationImp.h:
+
+/Applications/root_v5.34.34/include/TChain.h:
+
+/Applications/root_v5.34.34/include/TTree.h:
+
+/Applications/root_v5.34.34/include/TBranch.h:
+
+/Applications/root_v5.34.34/include/TDataType.h:
+
+/Applications/root_v5.34.34/include/TDictionary.h:
+
+/Applications/root_v5.34.34/include/Property.h:
+
+/Applications/root_v5.34.34/include/ESTLType.h:
+
+/Applications/root_v5.34.34/include/TClass.h:
+
+/Applications/root_v5.34.34/include/TObjString.h:
+
+/Applications/root_v5.34.34/include/ThreadLocalStorage.h:
+
+/Applications/root_v5.34.34/include/RConfigure.h:
+
+/Applications/root_v5.34.34/include/TVirtualTreePlayer.h:
+
+/Applications/root_v5.34.34/include/TCut.h:
+
+/Applications/root_v5.34.34/include/TH2.h:
+
+/Applications/root_v5.34.34/include/TMatrixFBasefwd.h:
+
+/Applications/root_v5.34.34/include/TMatrixDBasefwd.h:
+
+../src/../include/tree_leaves.h:

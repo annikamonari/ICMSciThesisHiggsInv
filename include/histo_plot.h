dd5127f5fec61705df78e96e2502c880d970e204
Annika Monari <annikamonari@gmail.com>
2015-11-23 19:18:35 +0000

    legend function finished

--- include/histo_plot.h
+++ include/histo_plot.h
@@ -18,17 +18,15 @@ public:
   static void draw_stacked_histo(Variable* var, std::vector<DataChain*> bg_chains, 
                                  DataChain* signal_chain, DataChain* data, bool with_cut);
 
-  static void set_y_max(TH1F* data, TH1F* background, THStack* hs);
+  static std::list<double> get_y_max(TH1F* data, TH1F* background);
 
   static std::list<TH1F*> get_histos_from_stack(THStack* hs);
 
   static void style_stacked_histo(THStack* hs, const char* x_label);
 
-  static double set_y_max(TH1F* data, TH1F* background);
-
   static void style_legend(TLegend* legend);
 
-  static std::list<double> legend_coords(TH1F* histo, Variable* var, bool with_cut);
+  static std::list<double> legend_coords(TH1F* histo, Variable* var, bool with_cut, double y_max);
 
   static int get_leg_overlap(TH1F* histo, TLegend* leg, int start_bin, int end_bin, double y1_gc);
 

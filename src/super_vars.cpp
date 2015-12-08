5cf206ca532059ceef4eeada0719a2352e6b0192
Annika Monari <annikamonari@gmail.com>
2015-12-08 06:37:22 +0000

    graphs for today finished

--- src/super_vars.cpp
+++ src/super_vars.cpp
@@ -2,25 +2,25 @@
 
 SuperVars::SuperVars()
 {
-		forward_tag_eta = new Variable("forward_tag_eta","ForwardTagEta","-5.0","5.0","1.8",
-																																	"5.0","60","50", true);
-		dijet_deta = new Variable("dijet_deta","DijetDeta","3.5","8.0","4.2","8.0","25","100");
-		metnomu_significance = new Variable("metnomu_significance","METExcludingMuonsSignificance",
-																																						"3.0","12.0","3.5","","50","70");
-		sqrt_ht = new Variable("sqrt_ht","SquareRootHCALScalarSumofEnergy","0.0","35.0","9.0",
-																									"18.0","75","50");
-		alljetsmetnomu_mindphi = new Variable("alljetsmetnomu_mindphi","Alljets,METmindphiexcludingmuons",
-																																								"0.0","3.5","0.5","3.0","40","30");
-		dijet_M = new Variable("dijet_M","DijetMass","0.0","2000.0","800.0","","50","100");
-		metnomuons = new Variable("metnomuons","METExcludingMuons","0.0","400.0","120.0","",
-																												"50","80");
+		forward_tag_eta = new Variable("forward_tag_eta","Forward Tag #eta","-5.0","5.0","1.8",
+																																	"5.0","60","30", true);
+		dijet_deta = new Variable("dijet_deta","Dijet #Delta#eta","3.5","8.0","4.2","8.0","25","20");
+		metnomu_significance = new Variable("metnomu_significance","MET Significance (No Muons)",
+																																						"3.0","12.0","3.5","","50","30");
+		sqrt_ht = new Variable("sqrt_ht","Square Root HCAL Scalar Sum of Energy","0.0","35.0","9.0",
+																									"18.0","75","10");
+		alljetsmetnomu_mindphi = new Variable("alljetsmetnomu_mindphi","All Jets - MET Min. #Delta#phi (No Muons)",
+																																								"0.0","3.5","0.5","3.0","40","20");
+		dijet_M = new Variable("dijet_M","Dijet Mass","0.0","2000.0","800.0","","50","20");
+		metnomuons = new Variable("metnomuons","MET (No Muons)","0.0","400.0","120.0","",
+																												"50","20");
 		jet1_E = new Variable("jet1_E","Jet1E","0.0","5000.0","50.0","","30","100");
 		jet2_E = new Variable("jet2_E","Jet2E","0.0","5000.0","45.0","","30","100");
 }
 
 std::vector<Variable*> SuperVars::get_discriminating_vars()
 {
-		Variable* var_arr[] = {alljetsmetnomu_mindphi};
+		Variable* var_arr[] = {alljetsmetnomu_mindphi, forward_tag_eta, dijet_deta, metnomu_significance, sqrt_ht, dijet_M, metnomuons};
 
 		std::vector<Variable*> vars (var_arr, var_arr + sizeof(var_arr) / sizeof(var_arr[0]));
 
@@ -29,7 +29,7 @@ std::vector<Variable*> SuperVars::get_discriminating_vars()
 
 std::vector<Variable*> SuperVars::get_signal_cut_vars()
 {
-		Variable* var_arr[] =		{metnomu_significance, alljetsmetnomu_mindphi, dijet_deta};
+		Variable* var_arr[] =		{metnomu_significance, alljetsmetnomu_mindphi, dijet_deta, jet1_E, jet2_E};
 																									/*{m_mumu, forward_tag_eta, dijet_deta, metnomu_significance, sqrt_ht, dijet_M,
 																									alljetsmetnomu_mindphi, metnomuons, jet1_pt, jet2_eta, central_tag_eta,
 																									dijet_dphi, metnomu_x, metnomu_y,  ht, ht30, unclustered_et, jet1metnomu_dphi,

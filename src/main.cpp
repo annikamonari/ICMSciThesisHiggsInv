03ee7c8da6d8bd8c0e37f5499bec4743510de5db
Annika Monari <annikamonari@gmail.com>
2015-12-14 17:39:47 +0000

    more edits for tmva

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 #include <initializer_list>
 #include <cmath>
-#include "../include/mc_weights.h"
-#include "../include/histo_plot.h"
-//#include "../include/bdt_analysis.h"
+#include "../include/bdt_analysis.h"
 
 void produce_graphs(bool with_cut) {
   SuperVars* super_vars             = new SuperVars();
   std::vector<Variable*> vars       = super_vars->get_discriminating_vars();
   std::vector<Variable*> cut_vars   = super_vars->get_signal_cut_vars();
-  super_vars->get_cuts_str_for_tmva();
+
   SuperChains* super_chains         = new SuperChains(&vars, &cut_vars, with_cut);
   std::vector<DataChain*> bg_chains = super_chains->get_bg_chains();
   DataChain* signal_chain           = super_chains->signal_chain;
   DataChain* data_chain             = super_chains->data_chain;
+
+  BDTAnalysis::create_BDT(bg_chains[0], signal_chain, &vars, super_vars->get_cuts_str_for_tmva());
 
   for (int i = 0; i < vars.size(); i++)
   {

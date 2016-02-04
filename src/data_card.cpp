f0569780c3a58284230ddb13e3a9ead3016d34d8
dangordon1 <dsg12@ic.ac.uk>
2016-02-04 14:15:52 +0000

    updated data card

--- src/data_card.cpp
+++ src/data_card.cpp
@@ -6,26 +6,35 @@ double get_signal_error(DataChain* signal_chain,variable* var, bool with_cut, st
   TH1F* signalh = HistoPlot::build_1d_histo(signal_chain, var, with_cut, false, "goff", variables);
   double total_signal = HistoPlot::get_histo_integral(bg, with_cut, var);
 
-  return std::pow(total_signal,0.5);
+  double sig_sqrt = std::pow(total_signal,0.5);
+
+  return 1+(sig_sqrt/total_signal);
 }
 
 double get_bg_errors(DataChain* data, std::vector<DataChain*> bg_chains,
                                  Variable* var, bool with_cut, std::vector<Variable*>* variables)
 {
   double bg_errors[bg_chains.size()];
-  for(int i=0; i<bg_chains.size(),i++){
-    bg_errors[i]=HistoPlot::single_bg_error(data, bg_chains, bg_chains[i], var, with_cut, variables);
+  double rates = get_rates(data, sbg_chains,  bg_chain,
+                                 var, bool with_cut, std::vector<Variable*>* variables);
+  for(int i=0; i<bg_chains.size();i++){
+    double weight = MCWeights::calc_mc_weight(data, bg_chains, bg_chain, var, with_cut, variables);
+    double N = get_histo_integral(bg, with_cut, var);//integral of single bg
+    bg_errors_no = HistoPlot::single_bg_error(data, bg_chains, bg_chains[i], var, with_cut, variables);
+    bg_errors[i] = 1+(bg_errors_no/(N*weight));
   }
   return bg_errors;
 }
-double get rates(DataChain* data, std::vector<DataChain*> bg_chains, DataChain* bg_chain,
+double get_rates(DataChain* data, std::vector<DataChain*> bg_chains, DataChain* bg_chain,
                                  Variable* var, bool with_cut, std::vector<Variable*>* variables)
 {
-  
-  for(int i=0; i<bg_chains.size(),i++){
-  double weight = MCWeights::calc_mc_weight(data, bg_chains, bg_chain, var, with_cut, variables);
-  double MC_N_S = get_histo_integral(bg, with_cut, var);
-
+  double rates[bg_chains.size()];
+ 
+  for(int i=0; i<bg_chains.size();i++){
+    double weight = MCWeights::calc_mc_weight(data, bg_chains, bg_chain, var, with_cut, variables);
+    double N = get_histo_integral(bg, with_cut, var);
+    rates[i]=N*weight;
+  }
 }
 int imax = 1;//bin number
 int jmax = bg_chains.size(); //number of backgrounds

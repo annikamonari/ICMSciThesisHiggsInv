de76f975d64e12009d9ff9c781d49880c3f84b5d
dangordon1 <dsg12@ic.ac.uk>
2016-02-04 14:59:41 +0000

    data card

--- include/data_card.h
+++ include/data_card.h
@@ -5,13 +5,16 @@
 
 class DataCard
 {
+
   static double get_signal_error(DataChain* signal_chain, Variable* var, bool with_cut, std::vector<Variable*>* variables);
 
-  static double* get_bg_errors(DataChain* data, std::vector<DataChain*> bg_chains,
+  static std::vector<double> get_bg_errors(DataChain* data, std::vector<DataChain*> bg_chains,
                                Variable* var, bool with_cut, std::vector<Variable*>* variables);
 
-  static double get_rates(DataChain* data, std::vector<DataChain*> bg_chains, DataChain* bg_chain,
+  static std::vector<double> get_rates(DataChain* data, std::vector<DataChain*> bg_chains, DataChain* bg_chain,
                           Variable* var, bool with_cut, std::vector<Variable*>* variables);
+
+  static std::vector<int> bin_line(std::vector<DataChain*> bg_chains);
 };
 
 #endif
--- src/data_card.cpp
+++ src/data_card.cpp
@@ -11,19 +11,23 @@ double DataCard::get_signal_error(DataChain* signal_chain, Variable* var, bool w
   return 1+(sig_sqrt/total_signal);
 }
 
-double* DataCard::get_bg_errors(DataChain* data, std::vector<DataChain*> bg_chains,
+ std::vector<double> DataCard::get_bg_errors(DataChain* data, std::vector<DataChain*> bg_chains,
                                  Variable* var, bool with_cut, std::vector<Variable*>* variables)
 {
-  double bg_errors[bg_chains.size()];
+  double bg_errors[std::vector<int> bin_line()
+{
+}
+];
   double rates = get_rates(data, bg_chains,var,with_cut, variables);
   for(int i=0; i<bg_chains.size();i++)
   {
     bg_errors_no = HistoPlot::single_bg_error(data, bg_chains, bg_chains[i], var, with_cut, variables);
     bg_errors[i] = 1+(bg_errors_no/rates[i]);
   }
-  return bg_errors;
+  std::vector<double> mc_weights_vector (bg_errors, bg_errors + sizeof(bg_errors) / sizeof(bg_errors[0]));
+  return bg_errors_vector;
 }
-double get_rates(DataChain* data, std::vector<DataChain*> bg_chains,
+std::vector<double> get_rates(DataChain* data, std::vector<DataChain*> bg_chains,
                                  Variable* var, bool with_cut, std::vector<Variable*>* variables)
 {//returns total number of each background as an array of doubles.
   double rates[bg_chains.size()];
@@ -31,11 +35,40 @@ double get_rates(DataChain* data, std::vector<DataChain*> bg_chains,
   for(int i=0; i<bg_chains.size();i++)
   {
     double weight = MCWeights::calc_mc_weight(data, bg_chains, bg_chains[i], var, with_cut, variables);
-    double N = get_histo_integral(bg_chains[i], with_cut, var);//integral of single bg
+    double N = HistoPlot::get_histo_integral(bg_chains[i], with_cut, var);//integral of single bg
     rates[i]=N*weight;
-  }  
-   return rates;
+  }
+  std::vector<double> rates_vector (rates, rates + sizeof(rates) / sizeof(rates[0]));
+  return rates_vector;
 }
+
+std::vector<int> bin_line(std::vector<DataChain*> bg_chains)
+{
+  int line_length = bg_chains.size()+1;// 1 for signal
+  int b_line[line_length];
+  for(int i=0;i<line_length;i++)
+  {
+   b_line[i] = 1; 
+  }
+  std::vector<int> bl_vector (b_line, b_line + sizeof(b_line) / sizeof(b_line[0]));
+  return bl_vector;
+}
+std::vector<int> process_line_2(std::vector<DataChain*> bg_chains)
+{
+  int line_length = bg_chains.size()+1;// 1 for signal
+  int p_line[line_length];
+  for(int i=0;i<line_length;i++)
+    {
+     p_line[i] = i; 
+    }
+  std::vector<int> pl_vector (p_line, p_line + sizeof(p_line) / sizeof(p_line[0]));
+  return pl_vector;
+
+}
+std::vector<int> rate_line()
+{
+}
+
 
 /*
 int imax = 1;//bin number

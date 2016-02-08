33479e2b2d39a99c17e8b177e008b2efe580f81d
Annika Monari <annikamonari@gmail.com>
2016-02-08 17:16:30 +0000

    datacard finished

--- src/data_card.cpp
+++ src/data_card.cpp
@@ -22,9 +22,7 @@ std::vector<double> DataCard::get_bg_errors(DataChain* data, std::vector<DataCha
 
   for(int i = 0; i < bg_chains.size(); i++)
   {
-   // std::cout<<"bg chain about to have error calculated"<<bg_chains[i]->legend<<"\n";
-    double bg_errors_val = HistoPlot::single_bg_error(data, bg_chains, bg_chains[i], var, with_cut, variables,bg_mc_weights[i]);
-    bg_errors[i] = 1+(bg_errors_val/rates[i+1]);
+    bg_errors_parsed[i] = 1 + (bg_errors[i] / rates[i+1]);
   }
   std::vector<double> bg_error_vector (bg_errors_parsed, bg_errors_parsed + sizeof(bg_errors_parsed) / sizeof(bg_errors_parsed[0]));
 
